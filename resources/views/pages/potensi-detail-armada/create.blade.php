a1490a223c7da9ea9e2b02f6e0ad5a2337c0f0f1
yogirzlsinatrya <yogirizalsinatrya@rocketmail.com>
2022-09-09 18:53:52 +0700

    works rutes

--- resources/views/pages/potensi-detail-armada/create.blade.php
+++ resources/views/pages/potensi-detail-armada/create.blade.php
@@ -50,7 +50,7 @@
                                     <a href="#" class="btn btn-icon btn-success" data-bs-toggle="modal" data-bs-target="#kt_modal_1"><i class="fas fa-add"></i></a>
                                 </div>
                             </div>  
-                            <div id="list_checkpoint"></div> 
+                            <div id="list_checkpoint" style="padding-top: 5px;"></div> 
                         </div>
                         <div class="col-md-6">
                             <div class="row">
@@ -60,9 +60,37 @@
                                 <div class="col-md-8">
                                     <input type="text" class="form-control input-sm" placeholder="">
                                 </div>
-                            </div>   
+                            </div>
+                            <div class="row" style="padding-top: 10px;">
+                                <div class="col-md-4">
+                                    <label class="form-label mt-2">Lat/Long</label>
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control input-sm" placeholder="Latitude">
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control input-sm" placeholder="Latitude">
+                                </div>
+                            </div> 
+                            <div class="row" style="padding-top: 10px;">
+                                <div class="col-md-12">
+                                    <a href="javascript:void(0)" class="btn btn-block btn-danger" id="create_rute">Generate Rutes</a>
+                                </div>
+                            </div>  
                         </div>
-                    </div>   
+                    </div>
+                    <hr style="border-top: 1px dotted black;">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div id="rute_map" style="height:500px;"></div>
+                        </div>
+                        <div class="col-md-4">
+                            <div id="sidebar">
+                                <p>Total Distance: <span id="total"></span></p>
+                                <div id="panel"></div>
+                            </div>
+                        </div>
+                    </div>  
                 <!-- end of card-body -->
                 </div>
             
@@ -149,7 +177,7 @@
 // delete checkpoint
 $(document).ready(function(){
     $(document).on('click', '.delete_rute', function(e) {
-        $(this).parent().parent().parent().parent().remove();
+        $(this).parent().parent().parent().remove();
     });
 
     $("#add_checkpoint").click(function(){
@@ -163,7 +191,7 @@ $(document).ready(function(){
                         '<label class="form-label mt-2">Rute </label>'+
                     '</div>'+
                     '<div class="col-md-6">'+
-                        '<input type="text" class="form-control input-sm" placeholder="" value="'+ lat +','+ lng +'">'+
+                        '<input name="checkpoint[]" type="text" class="form-control input-sm" placeholder="" value="'+ lat +','+ lng +'">'+
                     '</div>'+
                     '<div class="col-md-2">'+
                         '<a href="#" class="btn btn-icon btn-danger delete_rute"><i class="fas fa-times"></i></a>'+
@@ -171,16 +199,97 @@ $(document).ready(function(){
                 '</div>'+ 
             '</div>');
     });
+
+    $(document).on('click', '#create_rute', function(e) {
+
+        var waypts = [];
+        $("input[name='checkpoint[]']")
+            .map(function(){
+                var temp = $(this).val().split(',');
+                waypts.push({
+                    location: {
+                        lat: parseFloat(temp[0]),
+                        lng: parseFloat(temp[1])
+                    },
+                    stopover: true
+                });
+        });
+     
+        const map = new google.maps.Map(document.getElementById("rute_map"), {
+            zoom: 6,
+            center: { lat: -7.258621, lng: 112.750281 }, // Indonesia.
+        });
+        const directionsService = new google.maps.DirectionsService();
+        const directionsRenderer = new google.maps.DirectionsRenderer({
+            draggable: true,
+            map,
+            panel: document.getElementById("panel"),
+        });
+
+        directionsRenderer.addListener("directions_changed", () => {
+            const directions = directionsRenderer.getDirections();
+
+            if (directions) {
+                computeTotalDistance(directions);
+            }
+        });
+        displayRoute(
+            // "Surabaya, Surabaya City, East Java, Indonesia",
+            // "Sidoarjo, Sidoarjo Regency, East Java, Indonesia",
+            directionsService,
+            directionsRenderer
+        );
+        
+
+        function displayRoute(service, display) {  
+            service.route({
+                origin: { location: { lat: -6.215654, lng: 106.802097 } },
+                destination: { location: { lat: -7.258621, lng: 112.750281 } },
+                waypoints: waypts,  
+                // [ values
+                //     // { location: { lat: -7.258621, lng: 112.750281 } },
+                //     // { location: "Broken Hill, NSW" },
+                // ],
+                travelMode: google.maps.TravelMode.DRIVING,
+                avoidTolls: true,
+                })
+                .then((result) => {
+                    display.setDirections(result);
+                })
+                .catch((e) => {
+                    alert("Could not display directions due to: " + e);
+                });
+        }
+
+        function computeTotalDistance(result) {
+            let total = 0;
+            const myroute = result.routes[0];
+
+            if (!myroute) {
+                return;
+            }
+
+            for (let i = 0; i < myroute.legs.length; i++) {
+                total += myroute.legs[i].distance.value;
+            }
+
+            total = total / 1000;
+            document.getElementById("total").innerHTML = total + " km";
+        }
+
+        window.initMap = initMap;
+
+    });
 });
 </script>
 
 
-<script>
+<script type="text/javascript">
 	function initMap() {
-		var centerCoordinates = new google.maps.LatLng(37.6, -95.665);
+		var centerCoordinates = new google.maps.LatLng(-0.789275, 113.921327); // indonesia
 		var map = new google.maps.Map(document.getElementById('map'), {
 			center : centerCoordinates,
-			zoom : 4
+			zoom : 6
 		});
 		var card = document.getElementById('pac-card');
 		var input = document.getElementById('pac-input');
@@ -196,7 +305,6 @@ $(document).ready(function(){
 			map : map,
             draggable: true
 		});
-
 
 		autocomplete.addListener('place_changed',function() {
 			document.getElementById("location-error").style.display = 'none';
@@ -223,11 +331,6 @@ $(document).ready(function(){
 		});
 
         google.maps.event.addListener(marker, 'dragend', function (evt) {
-            // document.getElementById('current')
-            //     .innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(6) + ' Current Lng: ' + evt.latLng.lng().toFixed(6) + '</p>';
-            // // $('#checkpoint_lat').val('');
-            // // $('#checkpoint_lng').val('');
-
             $('#checkpoint_lat').val(evt.latLng.lat().toFixed(6));
             $('#checkpoint_lng').val(evt.latLng.lng().toFixed(6));
 
@@ -246,6 +349,5 @@ $(document).ready(function(){
 	}
 </script>
 <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC0f2vYkUlCd6XCyu17DBElvuxyf_4quCU&libraries=places&callback=initMap"
-    async defer></script>
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC0f2vYkUlCd6XCyu17DBElvuxyf_4quCU&libraries=places&callback=initMap"></script>
 @endsection
