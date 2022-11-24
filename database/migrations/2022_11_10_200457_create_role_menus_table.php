a86f12e992360ea1654c3b690ff963b44f18cd63
Choirul <irul@technesia.co>
2022-11-24 11:55:04 +0700

    update

--- database/migrations/2022_11_10_200457_create_role_menus_table.php
+++ database/migrations/2022_11_10_200457_create_role_menus_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('tms_role_menus', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained();
-            $table->foreignId('menu_id')->constrained();
+            $table->foreignId('role_id')->constrained('tms_roles');
+            $table->foreignId('menu_id')->constrained('tms_menus');
             $table->timestamps();
         });
     }
