24f713bceaae2f69d8e54e3259989eaf1ba73ba0
Choirul <irul@technesia.co>
2022-12-25 21:37:52 +0700

    .

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,6 +26,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $url = $this->app['url'];
+        // Force the application URL
+        $url->forceRootUrl(config('app.url'));
+}
         $this->configureRateLimiting();
 
         $this->routes(function () {
