8c41e8fe54559b4d4d21949e9c1a85e11eccc9c0
Choirul <irul@technesia.co>
2023-06-08 10:14:48 +0700

    update

--- resources/views/prints/sptb.blade.php
+++ resources/views/prints/sptb.blade.php
@@ -282,7 +282,9 @@
                     <td>
                         Dikirim oleh / <br>
                         <i>Deliver by</i><br>
-                        <img alt="Logo" src="data:image/png;base64, {{ $data->admDistribusi->signature_base_64 }}" class="logo" height="50px;"/><br>
+                        @if (!empty($data->spmh->admDistribusi))
+                            <img alt="Logo" src="data:image/png;base64, {{ $data->admDistribusi->signature_base_64 }}" class="logo" height="50px;"/><br>
+                        @endif
                         {{ !empty($data->spmh->admDistribusi)?$data->spmh->admDistribusi->full_name:"-" }}
                         <hr class="new1">
                         Adm Distribusi
