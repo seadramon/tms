cc98e304105c45ebfd0a282b5c78c186afa2fe61
Choirul <irul@technesia.co>
2023-06-20 08:51:02 +0700

    update

--- app/Http/Controllers/Api/Internal/InternalController.php
+++ app/Http/Controllers/Api/Internal/InternalController.php
@@ -106,7 +106,7 @@ class InternalController extends Controller
         if($request->tgl_awal && $request->tgl_akhir){
             $query->whereBetween('tgl_sptb', [date('Y-m-d 00:00:00', strtotime($request->tgl_awal)), date('Y-m-d 23:59:59', strtotime($request->tgl_akhir))]);
         }
-        $sptb = $query->get();
+        $sptb = $query->orderByRaw('app_pelanggan ASC NULLS FIRST')->orderBy('tgl_sptb', 'asc')->get();
 
 
         return SptbListResource::collection($sptb);
